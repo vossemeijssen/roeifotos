e6f2279ee4bf7235178c5865adc34ecb31dea8d1
vmeijssen <vmeijssen@deloitte.nl>
2022-06-10 12:20:28 +0200

    small merge

--- code/train_model.py
+++ code/train_model.py
@@ -4,6 +4,7 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torchvision.models as models
 
 
 resizedpath = os.path.join(os.getcwd(), 'resized')
@@ -17,20 +18,25 @@ is_laga_int = [int(is_laga_instance) for is_laga_instance in is_laga] # List of 
 is_laga_one_hot = [(1, 0) if i else (0, 1) for i in is_laga] # List of (1, 0) and (0, 1)
 file_names = club_labels["imagename"]
 
+resnet18 = models.resnet18(pretrained=True)
+print(resnet18)
 
 class Net(nn.Module):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(3, 3, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(3, 3, 5)
+        self.resnet18 = models.resnet18(pretrained=True)
+        for param in resnet18.parameters():
+            param.requires_grad = False
+        # self.conv1 = nn.Conv2d(3, 3, 5)
+        # self.pool = nn.MaxPool2d(2, 2)
+        # self.conv2 = nn.Conv2d(3, 3, 5)
         # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(3*100*100, 4)
+        # self.fc2 = nn.Linear(3*100*100, 4)
         self.fc3 = nn.Linear(4, 1)
 
     def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
+        # x = self.pool(F.relu(self.conv1(x)))
+        # x = self.pool(F.relu(self.conv2(x)))
         x = torch.flatten(x, 1) # flatten all dimensions except batch
         # x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
